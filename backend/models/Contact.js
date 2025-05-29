2b750503973916458ed0e66f7a1cf36383883279
Ajanta Das <ajanta744@gmail.com>
2025-05-29 14:16:21 +1000

    update file

---
+++ backend/models/Contact.js
@@ -0,0 +1,12 @@
+// backend/models/Contact.js
+
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
